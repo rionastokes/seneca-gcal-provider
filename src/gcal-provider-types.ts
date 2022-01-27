a6c2adbe4f660a92bf03e71dd07685156f4b80d9
Vitor Lelis <vitorclelis96@gmail.com>
2022-01-27 10:05:25 -0300

    gcal-provider plugin

---
+++ src/gcal-provider-types.ts
@@ -0,0 +1,13 @@
+type GCalTokenCredentials = {
+    installed: {
+        client_id: string,
+        project_id: string,
+        auth_uri: string,
+        token_uri: string,
+        auth_provider_x509_cert_url: string,
+        client_secret: string,
+        redirect_uris: string[]
+    }
+}
+
+export type { GCalTokenCredentials };
